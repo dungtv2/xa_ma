50699c252096b0f7321e061fc151000db61298d0
dungtv2 <dungtv2@hanelsoft.vn>
2016-07-11 09:30:42 +0700

    f

---
+++ change_string_field_form_view_ok_nha_me/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Dynamic FormView advance Odoo 9',
+    'summary': 'Dynamic FormView advance Odoo 9',
+    'version': '1.0',
+    'category': 'Web',
+    'description': """
+        Dynamic FormView advance Odoo 9
+    """,
+    'author': "HanelSoft",
+    'website': 'http://www.hanelsoft.vn/',
+    'images': ['images/ab.jpg'],
+    'depends': ['web'],
+    'data': ['templates.xml'],
+    'qweb': ['static/src/xml/formview.xml'],
+    'price': 165.00,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
